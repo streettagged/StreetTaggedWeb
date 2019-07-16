6815469a2bf5828abb5e42a4f88c6a9d8fbf648f
Dave Conroy <dconroy@gmail.com>
2019-07-16 03:02:36 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ class App extends Component {
     };
   }
   async componentDidMount() {
-    this.loadFacebookSDK();
   
     try {
       await Auth.currentAuthenticatedUser();
@@ -34,25 +33,6 @@ class App extends Component {
     }
   
     this.setState({ isAuthenticating: false });
-  }
-  
-  loadFacebookSDK() {
-    window.fbAsyncInit = function() {
-      window.FB.init({
-        appId            : config.social.FB,
-        autoLogAppEvents : true,
-        xfbml            : true,
-        version          : 'v3.1'
-      });
-    };
-  
-    (function(d, s, id){
-       var js, fjs = d.getElementsByTagName(s)[0];
-       if (d.getElementById(id)) {return;}
-       js = d.createElement(s); js.id = id;
-       js.src = "https://connect.facebook.net/en_US/sdk.js";
-       fjs.parentNode.insertBefore(js, fjs);
-     }(document, 'script', 'facebook-jssdk'));
   }
     
   userHasAuthenticated = authenticated => {
--- src/config.js
+++ src/config.js
@@ -1,32 +1,5 @@
-export default {
-
-  const dev = {
-    mapbox: {
-      TOKEN: "dummy"
-    },
-    s3: {
-      REGION: "us-east-1",
-      BUCKET: "streetart-test"
-    },
-    apiGateway: {
-      REGION: "us-east-1",
-      URL: "https://api-dev.streettagged.com"
-    },
-    cognito: {
-      REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_FJaKcw4bh",
-      APP_CLIENT_ID: "khbkequqcckarsm3j0ra4tu4p",
-      IDENTITY_POOL_ID: "us-east-1:d80cd3d4-0a8c-4cbf-a088-97e04bf4aef3"
-    },
-    social: {
-      FB: "336096600619048"
-    }
-  };
-
-  const prod = {
-    mapbox: {
-      TOKEN: "dummy"
-    },
+require('dotenv').config()
+const dev = {
     s3: {
       REGION: "us-east-1",
       BUCKET: "streetart-test"
@@ -41,7 +14,24 @@ export default {
       APP_CLIENT_ID: "khbkequqcckarsm3j0ra4tu4p",
       IDENTITY_POOL_ID: "us-east-1:d80cd3d4-0a8c-4cbf-a088-97e04bf4aef3"
     }
-  };
+};
+
+const prod = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "streetart-app-prod"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://api.streettagged.com"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_zlJ4M8wON",
+    APP_CLIENT_ID: "3r5e8gj7bg6p301n64dg1skhmj",
+    IDENTITY_POOL_ID: "us-east-1:6141af23-c913-4a8a-89c1-ec1ec00b20c4"
+  }
+};
   
   const config = process.env.REACT_APP_STAGE === 'production'
     ? prod
@@ -50,5 +40,8 @@ export default {
   export default {
     // Add common config values here
     MAX_ATTACHMENT_SIZE: 5000000,
+    mapbox: {
+      TOKEN: process.env.MAPBOX_TOKEN
+    },
     ...config
   };
