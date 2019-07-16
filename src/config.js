273326d4f1fa75b72603f25774cfe31411b2114d
Dave Conroy <dconroy@gmail.com>
2019-07-16 02:10:44 -0400

    first step devops

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-    MAX_ATTACHMENT_SIZE: 5000000,
+
+  const dev = {
     mapbox: {
       TOKEN: "dummy"
     },
@@ -19,12 +20,35 @@ export default {
     },
     social: {
       FB: "336096600619048"
-    },
-    s3public: {
-        bucketName: 'streetart-test',
-        dirName: 'photos',
-        region: 'us-east-1',
-        accessKeyId: 'AKIA5QKAX6BGIVXHRBPL',
-        secretAccessKey: 'WThBF5pSyGdvObZcth6AL3tj9HMlCe+PWR3BkgU/',
     }
+  };
+
+  const prod = {
+    mapbox: {
+      TOKEN: "dummy"
+    },
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "streetart-test"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://api-dev.streettagged.com"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_FJaKcw4bh",
+      APP_CLIENT_ID: "khbkequqcckarsm3j0ra4tu4p",
+      IDENTITY_POOL_ID: "us-east-1:d80cd3d4-0a8c-4cbf-a088-97e04bf4aef3"
+    }
+  };
+  
+  const config = process.env.REACT_APP_STAGE === 'production'
+    ? prod
+    : dev;
+  
+  export default {
+    // Add common config values here
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
   };
