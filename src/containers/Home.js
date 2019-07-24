625d7646a88a0b4c488e2733e8b67aa3e582700d
John O'Sullivan <jnosullivan@icloud.com>
2019-07-24 18:31:18 -0400

    Adding the favorite UI

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import { API } from "aws-amplify";
+import { API, Auth } from "aws-amplify";
 import { Link } from "react-router-dom";
 import { PageHeader, ListGroup, ListGroupItem, ControlLabel } from "react-bootstrap";
 import "./Home.css";
+
+import HeartCheckbox from 'react-heart-checkbox';
 
 export default class Home extends Component {
   constructor(props) {
@@ -30,29 +32,75 @@ export default class Home extends Component {
   }
 
   async streetart() {
-   const data = await API.get("street-art", "/art");
+   const { accessToken: { jwtToken } } = Auth.user.signInUserSession;
+
+   let body = {
+       body: {
+         "token":jwtToken
+       },
+       headers: {"Content-Type": "application/json"}
+   }
+
+   const data = await API.post("street-art", "/search/art", body);
    return data.artWorks;
+  }
+
+  onClick = (event, props) => {
+    console.log('props: ', props);
+    console.log('event: ', event);
   }
 
   handleStreetArtClick = event => {
     event.preventDefault();
     this.props.history.push(event.currentTarget.getAttribute("href"));
   }
-//header={streetart.name.trim().split("\n")[0]}
+
+onClickFavorite = async (i, e) => {
+  const { accessToken: { jwtToken } } = Auth.user.signInUserSession;
+  const { streetart } = this.state;
+  const list = streetart;
+
+  const item = list[i - 1];
+
+  if (item.isFavorited) {
+    let body = {
+        body: {
+          "token":jwtToken,
+          "artId": item.artId
+        },
+        headers: {"Content-Type": "application/json"}
+    }
+    await API.del("street-art", "/favorite", body);
+  } else {
+    let body = {
+        body: {
+          "token":jwtToken,
+          "artId": item.artId
+        },
+        headers: {"Content-Type": "application/json"}
+    }
+    await API.post("street-art", "/favorite", body);
+  }
+
+  list[i - 1].isFavorited = !list[i - 1].isFavorited;
+
+  this.setState({ streetart: list });
+}
+
 renderStreetArtList(streetart) {
   return ([{}].concat(streetart).map(
     (streetart, i) =>
       i !== 0
-        ? 
-          <ListGroupItem
-            key={streetart.artId}
-            href={`/art/${streetart.artId}`}
-            onClick={this.handleStreetArtClick}
-          > <div className="art-header">
-            {streetart.username}
-            </div>
-            <img src ={streetart.picture} alt="street art"  width="100%" />
-          </ListGroupItem>
+        ?
+         <div key={streetart.artId + ':' + i}>
+          <div className="art-header">
+           {streetart.username}
+          </div>
+          <img src ={streetart.picture} alt="street art"  width="100%" />
+          <div>
+            <button onClick={this.onClickFavorite.bind(this, i)}>{streetart.isFavorited ? "Hearted" : "Heart"}</button>
+          </div>
+         </div>
         : <ListGroupItem
             key="new"
             href="/art/new"
@@ -83,6 +131,11 @@ renderLander() {
   );
 }
 
+/*
+<ListGroup>
+  {!this.state.isLoading && this.renderStreetArtList(this.state.streetart)}
+</ListGroup>
+*/
 
 renderArt() {
   return (
