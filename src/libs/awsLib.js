230277ba82f475233b2ebcfe579c48823bcc1902
John O'Sullivan <jnosullivan@icloud.com>
2019-09-20 20:05:15 -0400

    Updating name

--- src/libs/awsLib.js
+++ src/libs/awsLib.js
@@ -1,8 +1,11 @@
-import { Storage } from "aws-amplify";
+import { Storage, Auth } from "aws-amplify";
+import uuidv4 from "uuidv4";
 
 export async function s3Upload(file) {
-  const filename = `${Date.now()}-${file.name}`;
-
+  const { attributes: { sub } } = Auth.user;
+  const filename = `${sub}-${uuidv4()}`;
+  console.log(filename);
+  
   const stored = await Storage.put(filename, file, {
     contentType: file.type,
     level: 'public',
